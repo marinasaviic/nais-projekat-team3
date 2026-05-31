0f6a8bd2bfbcb475fd40312cae84494d39c9a3d1
Marina Savic <marinasaviic@gmail.com>
2026-05-31 19:37:00 +0200

    fix: add missing timeseries application entrypoint

---
+++ NAIS-KT1-projekat/TimeseriesDatabaseService/src/main/java/timeseries/TimeseriesDatabaseServiceApplication.java
@@ -0,0 +1,12 @@
+package timeseries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeseriesDatabaseServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimeseriesDatabaseServiceApplication.class, args);
+    }
+}
